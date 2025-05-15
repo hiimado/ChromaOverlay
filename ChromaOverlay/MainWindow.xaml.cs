ad47223bb3932fa63ffb719bf29b9b3d999032f6
hiimado <adolfolunagat@gmail.com>
2025-05-14 20:12:07 -0400

    feat: initial system tray integration

--- ChromaOverlay/MainWindow.xaml.cs
+++ ChromaOverlay/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace ChromaOverlay
 {
@@ -16,9 +18,56 @@ namespace ChromaOverlay
     /// </summary>
     public partial class MainWindow : Window
     {
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayMenu;
+
         public MainWindow()
         {
+
             InitializeComponent();
+            SetupTray();
+        }
+
+        private void SetupTray()
+        {
+            trayMenu = new ContextMenuStrip();
+            trayMenu.Items.Add("Exit", null, OnExitClicked);
+
+            trayIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Application,
+                Visible = true,
+                Text = "ChromaOverlay",
+                ContextMenuStrip = trayMenu
+            };
+
+            trayIcon.DoubleClick += (s, e) =>
+            {
+                this.Show();
+                this.WindowState = WindowState.Normal;
+                this.Activate();
+            };
+        }
+
+        private void OnExitClicked(object? sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            base.OnStateChanged(e);
+            if (this.WindowState == WindowState.Minimized)
+            {
+                this.Hide();
+            }
         }
     }
 }
